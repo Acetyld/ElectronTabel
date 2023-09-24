04266745a77ad76c53562486c115556809727a08
Dion <dion@vitrion.nl>
2023-09-24 11:33:59 +0200

    Added git ignore and IDEA stuff Dion 4 minutes ago

---
+++ src/main/entities/Supplier.entity.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity({ name: 'Supplier' })
+export class Supplier {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'varchar' })
+  name: string
+}
